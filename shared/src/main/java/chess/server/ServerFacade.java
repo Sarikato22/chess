69023b6a007c81e65610eab444784b951eec6c20
Sara <sarahermosillac@gmail.com>
2025-11-10 14:05:44 -0700

    register and helper methods

--- shared/src/main/java/chess/server/ServerFacade.java
+++ shared/src/main/java/chess/server/ServerFacade.java
@@ -2,8 +2,15 @@ package chess.server;
 
 import chess.model.request.RegisterRequest;
 import chess.model.result.RegisterResult;
+import com.google.gson.Gson;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
@@ -12,10 +19,56 @@ public class ServerFacade {
     public ServerFacade(String url) {serverUrl = url; }
 
     public RegisterResult register(RegisterRequest req) throws ResponseException {
-
-
-        return RegisterResult result;
+        var request = buildRequest("POST", "/user", req);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
     }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
 
 }
 
